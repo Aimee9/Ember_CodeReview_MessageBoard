290e4e432b1cf979bb9506450d01e8b14e72e0be
Aimee <oem@oems-MacBook-Pro.local>
2015-10-04 23:24:48 -0700

    Delete a question and return user to the index

--- app/routes/question.js
+++ app/routes/question.js
@@ -4,4 +4,17 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
   },
+
+  actions: {
+    delete(question) {
+      if (confirm('This will completely erase this question and all its answers.  Are you sure you want to delete it?')) {
+        this.send('destroyQuestion', question);
+      }
+    },
+
+    destroyQuestion(question) {
+      question.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
